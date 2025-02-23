640eb3e7c389fd86b1381756f1465aaaf7884a3d
Xasquatch <bigfoot7774@gmail.com>
2025-02-23 16:34:16 +0900

    Initial commit

---
+++ src/main/java/com/forestfull/CorkNoteApplication.java
@@ -0,0 +1,13 @@
+package com.forestfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorkNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorkNoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/forestfull/CorkNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forestfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorkNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
