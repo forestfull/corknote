25920adbbe082ac8a174a6bc296a492bceba9d6b
hs.ko <tpsxja)!(@>
2025-02-27 12:52:02 +0900

    테스트 환경 구현 중

--- src/test/java/com/forestfull/config/JasyptConfigTest.java
+++ src/test/java/com/forestfull/config/JasyptConfigTest.java
@@ -1,24 +1,31 @@
 package com.forestfull.config;
 
+import com.forestfull.log.up.util.Log;
 import org.jasypt.encryption.StringEncryptor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 
 @SpringBootTest
 class JasyptConfigTest {
 
     private StringEncryptor encryptor;
 
+    @Autowired
     @Qualifier(value = "jasyptEncryptor")
     public void setEncryptor(StringEncryptor encryptor) {
         this.encryptor = encryptor;
     }
 
     private void encrypt(String message) {
+        Log.info(message, " = ", encryptor.encrypt(message));
     }
 
     private void decrypt(String message) {
+        Log.info(message, " = ", encryptor.decrypt(message));
     }
 
     @Test
