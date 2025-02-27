4855b0722861b4e5f6a2a65af6143da25b424afb
hs.ko <tpsxja)!(@>
2025-02-27 16:49:23 +0900

    기본 스타일로도 충분히 가능해서 수정

--- src/main/java/com/forestfull/router/MainController.java
+++ src/main/java/com/forestfull/router/MainController.java
@@ -1,10 +1,11 @@
 package com.forestfull.router;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MainController {
 
     @GetMapping("/")
