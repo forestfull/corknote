165305ac9b500d06dc62858517226d469c11eaa0
hs.ko <tpsxja)!(@>
2025-02-27 13:32:29 +0900

    jasypt 보완 및 암호화 완료

--- src/main/java/com/forestfull/config/JasyptConfig.java
+++ src/main/java/com/forestfull/config/JasyptConfig.java
@@ -3,6 +3,7 @@ package com.forestfull.config;
 
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.iv.RandomIvGenerator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +14,9 @@ public class JasyptConfig {
     @Bean(name = "jasyptEncryptor")
     StringEncryptor stringEncryptor(@Value("${jasypt.encryptor.password}") String password) {
         final PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        encryptor.setAlgorithm("PBEWithHMACSHA512AndAES_256");
         encryptor.setPassword(password);
+        encryptor.setIvGenerator(new RandomIvGenerator());
         encryptor.setPoolSize(1);
 
         return encryptor;
--- src/test/java/com/forestfull/config/JasyptConfigTest.java
+++ src/test/java/com/forestfull/config/JasyptConfigTest.java
@@ -1,18 +1,20 @@
 package com.forestfull.config;
 
 import com.forestfull.log.up.util.Log;
+import com.forestfull.prop.TunnelingProperties;
 import org.jasypt.encryption.StringEncryptor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
+
+import java.util.function.Supplier;
 
 @SpringBootTest
 class JasyptConfigTest {
 
     private StringEncryptor encryptor;
+    private TunnelingProperties tunnelingProperties;
 
     @Autowired
     @Qualifier(value = "jasyptEncryptor")
@@ -20,16 +22,43 @@ class JasyptConfigTest {
         this.encryptor = encryptor;
     }
 
-    private void encrypt(String message) {
-        Log.info(message, " = ", encryptor.encrypt(message));
+    @Autowired
+    public void setTunnelingProperties(TunnelingProperties tunnelingProperties) {
+        this.tunnelingProperties = tunnelingProperties;
     }
 
-    private void decrypt(String message) {
-        Log.info(message, " = ", encryptor.decrypt(message));
+    private void encrypt(Object message) {
+        Log.info(message, " = ENC(", encryptor.encrypt(String.valueOf(message)), ")");
+    }
+
+    private void decrypt(Object message) {
+        Log.info(message, " = ", encryptor.decrypt(String.valueOf(message)));
     }
 
     @Test
     void encrypt() {
-        encrypt("test");
+        final Supplier<TunnelingProperties> encryptionTunnelingProp = () -> {
+            TunnelingProperties.Ssh ssh = tunnelingProperties.getSsh();
+            TunnelingProperties.Database database = tunnelingProperties.getDatabase();
+
+            encrypt(database.getDriverClassName());
+            encrypt(database.getSchema());
+            encrypt(database.getPort());
+            encrypt(database.getUsername());
+            encrypt(database.getPassword());
+            System.out.println();
+            encrypt(ssh.getHost());
+            encrypt(ssh.getPort());
+            encrypt(ssh.getUsername());
+            encrypt(ssh.getPassword());
+            return tunnelingProperties;
+        };
+//        encryptionTunnelingProp.get();
+    }
+
+    @Test
+    void decrypt() {
+        Log.info(tunnelingProperties);
+
     }
 }
